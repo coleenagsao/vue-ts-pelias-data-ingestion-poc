bf539add0cc7189a04c06fe3594556d1dea0abc7
Coleen Agsao <caagsao@up.edu.ph>
2023-08-03 14:59:37 +0800

    docs: add README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-### Proof of Concept UI
+# Pelias Data Ingestion Proof of Concept (POC) UI
+
+## Description
+This Proof of Concept aims to test the feasibility of the flow of data ingestion for self-hosted Pelias: 
+
+1. Form Interface Input
+2. Pelias Document Model
+3. ElasticSearch API POST/PUT Request
+
+This aims to check if we can be able to replicate the pelias/csv-importer. We are able to ingest data in the pelias index, however it is not guaranteed to be 100% searchable. There is still something happening under the hood in the said csv-importer to guarantee searchability. 
+
+## Technologies Used
+![Vue.js](https://img.shields.io/badge/Vue.js-42b883?style=for-the-badge&logo=vue.js&logoColor=white)
+![TypeScript](https://img.shields.io/badge/TypeScript-2d79c7?style=for-the-badge&logo=typescript&logoColor=white)
+
+## Installation
+To run the project locally, follow these steps:
+
+1. Clone this repository.
+2. Navigate to the frontend directory: `cd frontend`
+3. Install the dependencies: `npm install`. 
+4. Do the same with the backend directory.
+4. Make sure your Pelias is running in your machine.
+5. Start the development server in the backend directory: `npm run dev`
+6. Start the website in the frontend directory: `npm run serve`
+
+Make sure you have self-hosted Pelias, Vue, and TS installed on your machine before proceeding with the installation.
--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -28,7 +28,7 @@ const placesTypes = [
 const getParents = async (req: Request) => {
     try {
       const response = await axios.get(`http://localhost:4000/v1/reverse?point.lat=${req.body.lat}&point.lon=${req.body.lon}`);
-      return response.data.features[0].properties;
+      return response.data;
     } catch (error) {
       throw new Error((error as Error).message);
     }
@@ -46,51 +46,99 @@ const getGID = (inputString: String) => {
         console.log("Not enough colons in the string to extract the desired part.");
     }
 }
-
+/* 
+custom:venue:139
+custom:venue:149
+zFXkkIkB_wftcQ4eJ6pM
+openstreetmap:venue:node/10984521006
+*/
 router.post("/api/data", async (req, res) => {
     try { 
-        var rev_add = await getParents(req);    // perform reverse geocoding
-        console.log(rev_add);
+        //.features[0].properties
+        var rev_add = await getParents(req);    // perform reverse geocoding 
         
+        if (rev_add.features.length != 0){        
+            rev_add = rev_add.features[0].properties;
+
+            console.log(rev_add);
+
+            // create the document
+            var doc = new Document( 'custom', 'venue', req.body.source_id)
+            .setName( 'default', req.body.name )
+            .addCategory( req.body.category )
+            .setCentroid({ lon: req.body.lon, lat: req.body.lat})
+
+            // add parent fields to document
+            placesTypes.forEach( (place) => {
+                const place_a = `${place}_a`;       // store country_a, locality_a
+                const place_gid = `${place}_gid`;   // store country_gid, locality_gid
+
+                // check if country, country_a, country_gid exists
+                if (place in rev_add && place_a in rev_add && place_gid in rev_add) {
+                    doc.addParent( place, rev_add[place], getGID(rev_add[place_gid]), rev_add[place_a])
+                }
+            });
+
+            console.log(doc); 
+
+            //directly stringify the inner object after removing the document wrapper
+            const data = JSON.stringify({...doc});
+            
+            // make the POST request
+            var uniqueID = ["custom", "venue", req.body.source_id].join(":");
+
+            axios.put('http://localhost:9200/pelias/_create/' + uniqueID, data, {
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            })
+            .then(response => {
+                console.log('Response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
+            });
+
+            res.send({message: doc});
+        } else {
+            res.send({message: "Can't reverse geocode"});
+        }
+    
+
+    } catch (err) {
+        res.status(500).json({error: "Error"});
+    }
+})
+
+router.post("/api/data1", async (req, res) => {
+    try { 
         // create the document
         var doc = new Document( 'custom', 'venue', req.body.source_id)
         .setName( 'default', req.body.name )
         .addCategory( req.body.category )
         .setCentroid({ lon: req.body.lon, lat: req.body.lat})
-
-        // add parent fields to document
-        placesTypes.forEach( (place) => {
-            const place_a = `${place}_a`;       // store country_a, locality_a
-            const place_gid = `${place}_gid`;   // store country_gid, locality_gid
-
-            // check if country, country_a, country_gid exists
-            if (place in rev_add && place_a in rev_add && place_gid in rev_add) {
-                doc.addParent( place, rev_add[place], getGID(rev_add[place_gid]), rev_add[place_a])
-            }
-        });
-
+        
         console.log(doc); 
 
         //directly stringify the inner object after removing the document wrapper
         const data = JSON.stringify({...doc});
-        
+            
         // make the POST request
         var uniqueID = ["custom", "venue", req.body.source_id].join(":");
 
         axios.put('http://localhost:9200/pelias/_create/' + uniqueID, data, {
             headers: {
             'Content-Type': 'application/json',
-            },
-        })
-        .then(response => {
-            console.log('Response:', response.data);
-        })
-        .catch(error => {
-            console.error('Error:', error.message);
+                },
+            })
+            .then(response => {
+                console.log('Response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
         });
 
         res.send({message: doc});
-
     } catch (err) {
         res.status(500).json({error: "Error"});
     }
